e44e02bf9ac2f4355c3aa2c93385f4c8d7a0d3b4
Megan Lavallee <megan.lavallee@yahoo.com>
2021-01-03 12:03:23 -0700

    routes

--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,43 @@
 // Import the ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
+// All functions
 const burger = {
+
+// Select All
   all: function(cb) {
     orm.all("burger", function(res) {
       cb(res);
     });
   },
+
+  // Add
+  insert: function (name, cb){
+    orm.insert("burger", "name", name, function (res) {
+      cb(res);
+    });
+  },
+
+  // Update
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  },
+
+  // Delete
+  delete: function(id, cb) {
+    orm.deleteOne("burger", id, cb);
+  },
+
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
     orm.create("burger", cols, vals, function(res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burger", objColVals, condition, function(res) {
-      cb(res);
-    });
-  }
+
+  
 };
 
 // Export the database functions for the controller
