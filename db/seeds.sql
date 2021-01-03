978b2293db3585a454e55e640410aa5b52dd4f61
Megan Lavallee <megan.lavallee@yahoo.com>
2021-01-03 12:02:23 -0700

    fixing schema

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
 INSERT INTO burger (name) VALUES ('place holder');
 INSERT INTO burger (name) VALUES ('place holder');
-INSERT INTO burger (name, devour) VALUES ('place holder', true);
-INSERT INTO burger (name, devour) VALUES ('place holder', true);
-INSERT INTO burger (name, devour) VALUES ('place holder', true);
+INSERT INTO burger (name, devour) VALUES ('place holder');
+INSERT INTO burger (name, devour) VALUES ('place holder');
+INSERT INTO burger (name, devour) VALUES ('place holder');
 INSERT INTO burger (name) VALUES ('place holder');
